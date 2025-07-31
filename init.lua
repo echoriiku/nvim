615275d9f0cf7316567bbf1fdd70f9435f69f898
Ricardo Rodrigues <42903366+echoriiku@users.noreply.github.com>
2025-07-31 18:54:11 -0300

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,32 +1,26 @@
 vim.o.winborder = "rounded"
-vim.opt.number = true                              -- Line numbers
-vim.opt.relativenumber = true                      -- Relative line numbers
-vim.opt.cursorline = true                          -- Highlight current line
-vim.opt.wrap = false                               -- Don't wrap lines
-vim.opt.scrolloff = 10                             -- Keep 10 lines above/below cursor
-vim.opt.sidescrolloff = 8                          -- Keep 8 columns left/right of cursor
-vim.opt.tabstop = 4                                -- Tab width
-vim.opt.termguicolors = true                       -- Enable 24-bit colors
-vim.opt.signcolumn = "yes"                         -- Always show sign column
-vim.opt.showmatch = true                           -- Highlight matching brackets
-vim.opt.matchtime = 2                              -- How long to show matching bracket
-vim.opt.swapfile = false                           -- Don't create swap files
-vim.opt.undofile = true                            -- Persistent undo
-vim.g.mapleader = " "                              -- Set leader key to space
+vim.o.number = true                              -- Line numbers
+vim.o.ignorecase = true                                      -- Ignore case
+vim.o.relativenumber = true                      -- Relative line numbers
+vim.o.cursorline = true                          -- Highlight current line
+vim.o.wrap = false                               -- Don't wrap lines
+vim.o.scrolloff = 10                             -- Keep 10 lines above/below cursor
+vim.o.sidescrolloff = 8                          -- Keep 8 columns left/right of cursor
+vim.o.tabstop = 2                                -- Tab width
+vim.o.termguicolors = true                       -- Enable 24-bit colors
+vim.o.signcolumn = "yes"                         -- Always show sign column
+vim.o.showmatch = true                           -- Highlight matching brackets
+vim.o.swapfile = false                           -- Don't create swap files
+vim.o.undofile = true                            -- Persistent undo
+vim.g.mapleader = " "                            -- Set leader key to space
 vim.cmd("set completeopt+=noselect")
-vim.opt.undodir = vim.fn.expand("~/.vim/undodir")  -- Undo directory
+vim.o.undodir = vim.fn.expand("~/.vim/undodir")  -- Undo directory
 -- Create undo directory if it doesn't exist
 local undodir = vim.fn.expand("~/.vim/undodir")
 if vim.fn.isdirectory(undodir) == 0 then
   vim.fn.mkdir(undodir, "p")
 end
 vim.pack.add({
-        { src = "https://github.com/vague2k/vague.nvim" },
-        { src = "https://github.com/echasnovski/mini.pick" }
+        { src = "https://github.com/vague2k/vague.nvim" }
 })
-require "mini.pick".setup()
-vim.keymap.set('n', '<leader>f', ":Pick files<CR>")
-vim.keymap.set('n', '<leader>h', ":Pick help<CR>")
-vim.keymap.set('n', '<leader>lf', vim.lsp.buf.format)
-require "vague".setup({ transparent = true })
 vim.cmd("colorscheme vague")
